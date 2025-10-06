eb76bfb0b8d96a9a42a1563f30a0bc3296b65465
Konstantin Poleshchuk <poleshchuk.konstantin@gmail.com>
2025-10-06 14:32:32 +0400

    pause menu

--- levels/base_level.gd
+++ levels/base_level.gd
@@ -2,6 +2,7 @@ extends BaseGameScene
 class_name BaseLevel
 
 const DIALOG_CONVERSATION = preload("res://game_kit/dialogs/dialog_conversation/dialog_conversation.tscn")
+const DIALOG_PAUSE = preload("res://game_kit/dialogs/dialog_pause.tscn")
 
 @onready var music: AudioStreamPlayer = %Music
 @onready var player: CharacterBody2D = $Player
@@ -15,6 +16,15 @@ func _ready() -> void:
 	camera_man.follow_target(player)
 	camera_man.zoom_to(0.8)
 	get_window().grab_focus()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		var result = await dialog_manager.open_dialog(DIALOG_PAUSE, {}, true)
+
+		if result.has("exit"):
+			get_tree().quit()
+			return
+
 
 func _on_level_finish_body_entered(_body: Node2D) -> void:
 	finished.emit({})
@@ -33,3 +43,7 @@ func handle_player_killed() -> void:
 
 func handle_dialog_zone_player_entered(dialog_name: String, conversation: Conversation) -> void:
 	var _result = await dialog_manager.open_dialog(DIALOG_CONVERSATION, {"conversation": conversation}, true)
+
+
+func _on_music_finished() -> void:
+	music.play()
